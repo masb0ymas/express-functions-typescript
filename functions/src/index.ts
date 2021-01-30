b87a5a2629cd25e41c380998a125a753bcd5fe82
masb0ymas <fajriajjha@gmail.com>
2021-01-30 11:01:21 +0700

    fix: app function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,15 @@
+import 'module-alias/register'
+import './pathAlias'
+
 import express from 'express'
 import cors from 'cors'
 import logger from 'morgan'
 import firebase from 'firebase'
-import functions from 'firebase-functions'
-import initialDatabase from 'config/firebase'
-import indexRoutes from 'routes'
+import * as functions from 'firebase-functions'
+import initialDatabase from '@config/firebase'
+import indexRoutes from '@routes/index'
+import ExpressErrorResponse from '@middlewares/ExpressErrorResponse'
+import { winstonStream } from '@config/winston'
 
 firebase.initializeApp(initialDatabase)
 
@@ -12,10 +17,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(logger('dev'))
+app.use(logger('combined', { stream: winstonStream }))
 
 // initial routes
 app.use(indexRoutes)
+
+app.use('/v1', ExpressErrorResponse)
 
 const api = functions.region('asia-southeast2').https.onRequest(app)
 export { api }

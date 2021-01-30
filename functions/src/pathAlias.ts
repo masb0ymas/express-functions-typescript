9c0873a262f8b7bb1cfe8bfa3eb521e2b055ae8d
masb0ymas <fajriajjha@gmail.com>
2021-01-30 11:01:02 +0700

    feat: add path alias

---
+++ functions/src/pathAlias.ts
@@ -0,0 +1,16 @@
+import * as ModuleAlias from 'module-alias'
+
+ModuleAlias.addAliases({
+  '@bin': `${__dirname}/bin`,
+  '@config': `${__dirname}/config`,
+  '@constants': `${__dirname}/constants`,
+  '@controllers': `${__dirname}/controllers`,
+  '@helpers': `${__dirname}/helpers`,
+  '@middlewares': `${__dirname}/middlewares`,
+  '@migrations': `${__dirname}/migrations`,
+  '@models': `${__dirname}/models`,
+  '@modules': `${__dirname}/modules`,
+  '@routes': `${__dirname}/routes`,
+  '@utils': `${__dirname}/utils`,
+  '@views': `${__dirname}/views`,
+})

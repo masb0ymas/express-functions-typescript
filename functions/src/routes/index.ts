037b2f2e702b5ea5262e909a7426a751bf5debca
masb0ymas <fajriajjha@gmail.com>
2021-01-30 11:00:54 +0700

    fix: indexing routes

--- functions/src/routes/index.ts
+++ functions/src/routes/index.ts
@@ -1,9 +1,21 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import BuildResponse from '@modules/Response/BuildResponse'
+import ResponseError from '@modules/Response/ResponseError'
 
 const router = express.Router()
 
-router.get('/', function (req: Request, res: Response) {
-  return res.status(200).json({ message: 'Express TS, Powered by Nusantech' })
+/* Home Page. */
+router.get('/', function (req: Request, res: Response, next: NextFunction) {
+  const buildResponse = BuildResponse.get({
+    message: 'Express Functions TS, Powered by masb0ymas',
+    github: 'https://github.com/masb0ymas/express-functions-typescript',
+  })
+  return res.json(buildResponse)
+})
+
+/* Forbidden Page. */
+router.get('/v1', function (req: Request, res: Response, next: NextFunction) {
+  throw new ResponseError.Forbidden('forbidden, wrong access endpoint')
 })
 
 export default router

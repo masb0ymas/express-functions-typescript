ba6526ab1a6ce4373855991f6330247b556c1bbf
masb0ymas <fajriajjha@gmail.com>
2021-02-03 21:57:47 +0700

    fix: import auth route

--- functions/src/routes/public.ts
+++ functions/src/routes/public.ts
@@ -5,4 +5,6 @@ const router = express.Router()
 export default router
 
 // List require route controllers
+require('@controllers/Auth/controller')
+require('@controllers/User/controller')
 require('@controllers/Role/controller')
